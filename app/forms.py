f1273b5afa94d0972fa18395bd91ab426ad10339
cermak <pcermak@live.com>
2020-05-07 13:08:05 +0200

    [Fix] Remove testtime

--- app/forms.py
+++ app/forms.py
@@ -487,8 +487,6 @@ class ExperimentsForm(forms.ModelForm):
                                 input_formats=('%H:%M',), required=False)
     endtime = forms.TimeField(widget=forms.TimeInput(format = '%H:%M'), 
                                 input_formats=('%H:%M',), required=False)
-    testtime = forms.TimeField(widget=forms.TimeInput(format = '%H:%M'), 
-                                input_formats=('%H:%M',), required=True)
     shared_options = forms.ModelMultipleChoiceField(required=False, queryset=SharedOptions.objects.none())
     
     class Meta:
@@ -588,7 +586,7 @@ class ExperimentsForm(forms.ModelForm):
                 None, 'description', 'local_contact',
             ),
             Fieldset(
-                None, 'local_contact_fixed', 'testtime'
+                None, 'local_contact_fixed',
             ),
             DateRangeField('Date', 'start', 'end', 'starttime', 'endtime'),
             ButtonHolder(
